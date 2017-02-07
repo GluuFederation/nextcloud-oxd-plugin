f6db2007a5868e81b63d7ed8070ec862f0c306d4
dollar007 <vlad.karapetyan.1988@mail.ru>
2017-02-07 23:09:06 +0400

    adding openid config page template

---
+++ gluusso/templates/content/indexopenidconfig.php
@@ -0,0 +1,238 @@
+<?php
+	/**
+	 * @copyright Copyright (c) 2016, nextcloud, Inc.
+	 *
+	 * @author Joas Schilling <coding@schilljs.com>
+	 *
+	 * @license AGPL-3.0
+	 *
+	 * This code is free software: you can redistribute it and/or modify
+	 * it under the terms of the GNU Affero General Public License, version 3,
+	 * as published by the Free Software Foundation.
+	 *
+	 * This program is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	 * GNU Affero General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU Affero General Public License, version 3,
+	 * along with this program.  If not, see <http://www.gnu.org/licenses/>
+	 *
+	 */
+	
+	/** @var \OCP\IL10N $l */
+	/** @var array $_ */
+	$base_url                   = $_['base_url'];
+	$get_scopes                 = $_['gluu_scopes'];
+	$gluu_config                = $_['gluu_config'];
+	$gluu_acr                   = $_['gluu_acr'];
+	$gluu_auth_type             = $_['gluu_auth_type'];
+	$gluu_send_user_check       = $_['gluu_send_user_check'];
+	$gluu_provider              = $_['gluu_provider'];
+	$gluu_user_role             = $_['gluu_user_role'];
+	$gluu_custom_logout         = $_['gluu_custom_logout'];
+	$gluu_new_roles             = $_['gluu_new_roles'];
+	$gluu_users_can_register    = $_['gluu_users_can_register'];
+	$gluu_is_oxd_registered     = $_['gluu_is_oxd_registered'];
+	$groups                     = $_['groups'];
+	
+	$message_error              = $_['message_error'];
+	$message_success            = $_['message_success'];
+	$openid_error               = $_['openid_error'];
+
+?>
+
+<div class="mo2f_container">
+    <div class="container">
+        <div id="messages">
+            <?php if (!empty($message_error)){ ?>
+                <div class="mess_red_error">
+                    <?php echo $message_error; ?>
+                </div>
+                <?php } ?>
+            <?php if (!empty($message_success)) { ?>
+                <div class="mess_green">
+                    <?php echo $message_success; ?>
+                </div>
+                <?php } ?>
+        </div>
+        <br/>
+        <ul class="navbar navbar-tabs">
+            <li id="account_setup"><a href="<?php echo $base_url;?>">General</a></li>
+            <?php if ( !$gluu_is_oxd_registered) {?>
+                <li class="active" id="social-sharing-setup"><button disabled >OpenID Connect Configuration</button></li>
+            <?php }else {?>
+                <li class="active" id="social-sharing-setup"><a href="<?php echo $base_url;?>openidconfig/">OpenID Connect Configuration</a></li>
+            <?php }?>
+            <li id=""><a data-method="#configopenid" href="https://oxd.gluu.org/docs/plugin/nextcloud/" target="_blank">Documentation</a></li>
+        </ul>
+        <div class="container-page">
+            <div id="configopenid" style="padding: 20px !important;">
+                <form action="<?php echo $base_url;?>gluupostdata" method="post" id="scpe_update">
+                    <input type="hidden" name="form_key" value="openid_config_page"/>
+                    <fieldset style="border: 2px solid #53cc6b; padding: 20px">
+                        <legend style="border-bottom:none; width: 110px !important;">
+                            <img style=" height: 45px;" src="<?php echo image_path('gluusso', 'gl1.png'); ?>"/>
+                        </legend>
+                        <h1 style="margin-left: 30px;padding-bottom: 20px; border-bottom: 2px solid black; width: 75% ">User Scopes</h1>
+                        <div >
+                            <table style="margin-left: 30px" class="form-table">
+                                <tr style="border-bottom: 1px solid green !important;">
+                                    <th style="width: 200px; padding: 0px">
+                                        <p style="text-align: left;" id="scop_section">
+                                            Requested scopes
+                                            <a data-toggle="tooltip" class="tooltipLink" data-original-title="Scopes are bundles of attributes that the OP stores about each user. It is recommended that you request the minimum set of scopes required">
+                                                <span class="glyphicon glyphicon-info-sign"></span>
+                                            </a>
+                                        </p>
+                                    </th>
+                                    <td style="width: 200px; padding-left: 10px !important">
+                                        <table id="table-striped" class="form-table" >
+                                            <tbody style="width: inherit !important;">
+                                            <tr style="padding: 0px">
+                                                <td style="padding: 0px !important; width: 10%">
+                                                    <p >
+                                                        <input checked type="checkbox" name=""  id="openid" value="openid"  disabled />
+
+                                                    </p>
+                                                </td>
+                                                <td style="padding: 0px !important; width: 70%">
+                                                    <p >
+                                                        <input type="hidden"  name="scope[]"  value="openid" />openid
+                                                    </p>
+                                                </td>
+                                                <td style="padding: 0px !important;  width: 20%">
+                                                    <a class="btn btn-danger btn-xs" style="margin: 5px; float: right" disabled><span class="glyphicon glyphicon-trash"></span></a>
+                                                </td>
+                                            </tr>
+                                            <tr style="padding: 0px">
+                                                <td style="padding: 0px !important; width: 10%">
+                                                    <p >
+                                                        <input checked type="checkbox" name=""  id="email" value="email"  disabled />
+
+                                                    </p>
+                                                </td>
+                                                <td style="padding: 0px !important; width: 70%">
+                                                    <p >
+                                                        <input type="hidden"  name="scope[]"  value="email" />email
+                                                    </p>
+                                                </td>
+                                                <td style="padding: 0px !important;  width: 20%">
+                                                    <a class="btn btn-danger btn-xs" style="margin: 5px; float: right" disabled><span class="glyphicon glyphicon-trash"></span></a>
+                                                </td>
+                                            </tr>
+                                            <tr style="padding: 0px">
+                                                <td style="padding: 0px !important; width: 10%">
+                                                    <p >
+                                                        <input checked type="checkbox" name=""  id="profile" value="profile"  disabled />
+
+                                                    </p>
+                                                </td>
+                                                <td style="padding: 0px !important; width: 70%">
+                                                    <p >
+                                                        <input type="hidden"  name="scope[]"  value="profile" />profile
+                                                    </p>
+                                                </td>
+                                                <td style="padding: 0px !important;  width: 20%">
+                                                    <a class="btn btn-danger btn-xs" style="margin: 5px; float: right" disabled><span class="glyphicon glyphicon-trash"></span></a>
+                                                </td>
+                                            </tr>
+                                            <?php foreach($get_scopes as $scop) :?>
+                                                <?php if ($scop == 'openid' or $scop == 'email' or $scop == 'profile'){?>
+                                                <?php } else{?>
+                                                    <tr style="padding: 0px">
+                                                        <td>
+                                                            <p id="<?php echo $scop;?>1">
+                                                                <input <?php if($gluu_config && in_array($scop, $gluu_config['config_scopes'])){ echo "checked";} ?> type="checkbox" name="scope[]"  id="<?php echo $scop;?>1" value="<?php echo $scop;?>" <?php if (!$gluu_is_oxd_registered || $scop=='openid') echo ' disabled '; ?> />
+                                                            </p>
+                                                        </td>
+                                                        <td>
+                                                            <p id="<?php echo $scop;?>1">
+                                                                <?php echo $scop;?>
+                                                            </p>
+                                                        </td>
+                                                        <td style="padding: 0px !important; ">
+                                                            <button type="button" class="btn btn-danger btn-xs delete_scopes" style="margin: 5px; float: right" value="<?php echo $scop;?>" ><span class="glyphicon glyphicon-trash"></span></button>
+                                                        </td>
+                                                    </tr>
+                                                <?php } endforeach;?>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr style="border-bottom: 1px solid green !important;">
+                                    <th>
+                                        <p style="text-align: left;" id="scop_section1">
+                                            Add scopes
+                                        </p>
+                                    </th>
+                                    <td>
+                                        <div style="margin-left: 10px" id="p_scents">
+                                            <p>
+                                                <input <?php if(!$gluu_is_oxd_registered) echo 'disabled'?> class="form-control" type="text" id="new_scope_field" name="new_scope[]" placeholder="Input scope name" />
+                                            </p>
+                                            <br/>
+                                            <p>
+                                                <input type="button" style="width: 80px" class="btn btn-primary btn-large" value="Add" id="add_new_scope"/>
+                                            </p>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <br/>
+                        <h1 style="margin-left: 30px;padding-bottom: 20px; border-bottom: 2px solid black; width: 75%">Authentication</h1>
+                        <br/>
+                        <p style=" margin-left: 30px !important; font-weight:bold "><label style="display: inline !important; "><input type="checkbox" name="send_user_check" id="send_user" value="1" <?php if(!$gluu_is_oxd_registered) echo 'disabled'?> <?php if( $gluu_send_user_check) echo 'checked';?> /> <span>Bypass the local NextCloud login page and send users straight to the OP for authentication</span></label>
+                        </p>
+                        <br/>
+                        <div>
+                            <table style="margin-left: 30px" class="form-table">
+                                <tbody>
+                                <tr>
+                                    <th style="width: 200px; padding: 0px; ">
+                                        <p style="text-align: left;" id="scop_section">
+                                            Select ACR: <a data-toggle="tooltip" class="tooltipLink" data-original-title="The OpenID Provider may make available multiple authentication mechanisms. To signal which type of authentication should be used for access to this site you can request a specific ACR. To accept the OP's default authentication, set this field to none.">
+                                                <span class="glyphicon glyphicon-info-sign"></span>
+                                            </a>
+                                        </p>
+                                    </th>
+                                    <td >
+                                        <?php
+                                            $custom_scripts = $gluu_acr;
+                                            if(!empty($custom_scripts)){
+                                                ?>
+                                                <select style="margin-left: 5px; padding: 0px !important; width: 200px"  name="send_user_type" id="send_user_type" <?php if(!$gluu_is_oxd_registered) echo 'disabled'?>>
+                                                    <option value="default">none</option>
+                                                    <?php
+                                                        if($custom_scripts){
+                                                            foreach($custom_scripts as $custom_script){
+                                                                if($custom_script != "default" and $custom_script != "none"){
+                                                                    ?>
+                                                                    <option <?php if($gluu_auth_type == $custom_script) echo 'selected'; ?> value="<?php echo $custom_script;?>"><?php echo $custom_script;?></option>
+                                                                    <?php
+                                                                }
+                                                            }
+                                                        }
+                                                    ?>
+                                                </select>
+                                            <?php } ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th >
+                                        <input type="submit" class="btn btn-primary btn-large" <?php if(!$gluu_is_oxd_registered) echo 'disabled'?> value="Save Authentication Settings" name="set_oxd_config" />
+                                    </th>
+                                    <td>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
